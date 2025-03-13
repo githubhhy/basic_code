bf81e65420f3c8a2bc1d8a58f6cd3ed9a54e288c
githubhhy <2303862532@qq.com>
2025-03-13 22:20:31 +0800

    20250313

--- FirstGame/FirstGame/src/ui/GameJFrame.java
+++ FirstGame/FirstGame/src/ui/GameJFrame.java
@@ -11,8 +11,13 @@ import javax.swing.JMenuItem;
 import javax.swing.WindowConstants;
 import javax.swing.border.BevelBorder;
 
-public class GameJFrame extends JFrame {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GameJFrame extends JFrame implements KeyListener {
     int[][] num = new int[4][4];
+    int x;
+    int y;
 
     // 表示游戏主界面，跟游戏相关逻辑在此处
     public GameJFrame() {
@@ -20,8 +25,13 @@ public class GameJFrame extends JFrame {
         initJFrame();
         // 初始化菜单
         initJMenuBar();
+        // 初始化位置
+        initArray();
         // 初始化图片
         initImage();
+
+        // 添加按键监听
+        this.addKeyListener(this);
 
         this.setVisible(true); // 界面可视
     }
@@ -57,7 +67,7 @@ public class GameJFrame extends JFrame {
         this.setJMenuBar(jMenuBar);
     }
 
-    private void initImage() {
+    private void initArray() {
         int[] temp_arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
         Random random = new Random();
         // 打乱
@@ -71,27 +81,34 @@ public class GameJFrame extends JFrame {
         for (int i = 0; i < num.length; i++) {
             for (int j = 0; j < num[0].length; j++) {
                 num[i][j] = temp_arr[i * 4 + j];
+                if (num[i][j] == 0) {
+                    x = i;
+                    y = j;
+                }
             }
         }
-        // 打印
-        // for (int i = 0; i < num.length; i++) {
-        // for (int j = 0; j < num[0].length; j++) {
-        // System.out.printf("%d, " ,num[i][j]);
-        // }
-        // System.out.println();
-        // }
+    }
 
+    private void initImage() {
+        this.getContentPane().removeAll();
+
+        //胜利，显示祝贺图
+        if (cheakWin()) {
+            JLabel win = new JLabel(new ImageIcon("FirstGame\\image\\win.png"));
+            win.setBounds(203, 283, 197, 73);
+            this.getContentPane().add(win);
+        }
 
         // 放置
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 // 创建JLable对象（即管理容器）
                 JLabel jLabel = new JLabel(
-                        new ImageIcon("FirstGame\\FirstGame\\image\\animal\\animal3\\" + num[i][j] + ".jpg"));// 创建ImageIcon对象
+                        new ImageIcon("FirstGame\\image\\animal\\animal3\\" + num[i][j] + ".jpg"));// 创建ImageIcon对象
 
                 // 指定位置
                 jLabel.setBounds(105 * j + 83, 105 * i + 134, 105, 105);
-                //给图片添加边框
+                // 给图片添加边框
                 jLabel.setBorder(new BevelBorder(1));
                 // 把管理容器添加到界面中
                 this.getContentPane().add(jLabel);
@@ -99,9 +116,117 @@ public class GameJFrame extends JFrame {
         }
 
         // 添加背景图片
-        JLabel background = new JLabel(new ImageIcon("FirstGame\\FirstGame\\image\\background.png"));
+        JLabel background = new JLabel(new ImageIcon("FirstGame\\image\\background.png"));
         background.setBounds(40, 40, 508, 560);
         this.getContentPane().add(background);
+
+        this.getContentPane().repaint();
     }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int a = e.getKeyCode();
+        if (a == 65) {
+            System.out.println("a:查看原图");
+            this.getContentPane().removeAll();
+            JLabel allimage = new JLabel(new ImageIcon("FirstGame\\image\\animal\\animal3\\all.jpg"));
+            allimage.setBounds(83, 134, 420, 420);
+            this.getContentPane().add(allimage);
+        }
+        // 添加背景图片
+        JLabel background = new JLabel(new ImageIcon("FirstGame\\image\\background.png"));
+        background.setBounds(40, 40, 508, 560);
+        this.getContentPane().add(background);
+
+        this.getContentPane().repaint();
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (cheakWin()) {
+            return;
+        }
+        
+        int i = e.getKeyCode();
+        switch (i) {
+            case 37:
+                System.out.println("左");
+                // 空白块和右边的块交换
+                if (y == 3)
+                    return;
+                num[x][y] = num[x][y + 1];
+                num[x][y + 1] = 0;
+                y = y + 1;
+                break;
+            case 38:
+                System.out.println("上");
+                // 空白块和下边的块交换
+                if (x == 3)
+                    return;
+                num[x][y] = num[x + 1][y];
+                num[x + 1][y] = 0;
+                x = x + 1;
+                break;
+            case 39:
+                System.out.println("右");
+                // 空白块和左边的块交换
+                if (y == 0)
+                    return;
+                num[x][y] = num[x][y - 1];
+                num[x][y - 1] = 0;
+                y = y - 1;
+                break;
+            case 40:
+                System.out.println("下");
+                // 空白块和上边的块交换
+                if (x == 0)
+                    return;
+                num[x][y] = num[x - 1][y];
+                num[x - 1][y] = 0;
+                x = x - 1;
+                break;
+            case 65:
+                System.out.println("a:取消查看");
+                break;
+            case 87:
+                System.out.println("w:作弊");
+                setWin();
+                break;
+            default:
+                break;
+        }
+        initImage();
+    }
+
+    private boolean cheakWin() {
+        int[][] temp = new int[][] {
+            { 1, 2, 3, 4 },
+            { 5, 6, 7, 8 },
+            { 9, 10, 11, 12 },
+            { 13, 14, 15, 0 }
+        };
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num[0].length; j++) {
+                if (num[i][j] != temp[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void setWin() {
+        num = new int[][] {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 0 }
+        };
+        initImage();
+    }
 }
